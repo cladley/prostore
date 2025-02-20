f2e9e49a07c13e0006e5ef181e6f54772a907a1c
Colin Ladley <colin.ladley@dotcraft.agency>
2025-02-20 16:14:09 +0000

    Added sign in

--- db/seed.ts
+++ db/seed.ts
@@ -6,8 +6,13 @@ async function main() {
 
   // Clear the product table
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
   // Create the products
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database seeded successfully");
 }
